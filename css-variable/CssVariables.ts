57763ddb990d25f5599d84a91a2620db7b542c7f
Okogod <godspowerokoro17@gmail.com>
2025-08-26 10:37:56 +0100

    First commit: Add Avenir roman and medium custom font, add screens, components, static data, navigations, css variable, folders

---
+++ css-variable/CssVariables.ts
@@ -0,0 +1,16 @@
+
+// Colors
+export enum ColorsVariables {
+    GreenColor = "#006B3F",
+    BlackColor = "#000B0F",
+    GreyColor = "#545454",
+    AshColor = "#F5F5F7",
+    YellowColor = "#BCD848",
+    RedColor = "#C54B4F"
+}
+
+// Font Family
+export enum FontFamily {
+    AvenirRoman = "Avenir Roman",
+    AvenirMedium = "Avenir Medium"
+}
---
+++ static/ImageData.ts
@@ -0,0 +1,15 @@
+
+
+export const Logo = require('../assets/images/logo.png');
+
+export const JoinImage = require('../assets/images/join-images/join-image.png');
+
+
+export const BackArrowIcon = require('../assets/images/icons/back-arrow-icon.png');
+export const DropDownIcon = require('../assets/images/icons/drop-down-icon.png');
+export const DropUpIcon = require('../assets/images/icons/drop-up-icon.png');
+export const VerifiedIcon = require('../assets/images/icons/verified-icon.png');
+
+
+export const CheckedBox = require('../assets/images/interfaces/checked-box.png');
+export const UncheckedBox = require('../assets/images/interfaces/unchecked-box.png')
